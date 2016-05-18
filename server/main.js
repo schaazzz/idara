70101e9800d9983f34b61e1f317865b389b3a59b
Shahzeb Ihsan <shahzeb@gmail.com>
2016-05-18 18:46:24 +0800

    .....

--- server/main.js
+++ server/main.js
@@ -3,3 +3,4 @@ import '../imports/api/projects.js';
 import '../imports/api/issues.js';
 import '../imports/api/comments.js';
 import '../imports/api/workflows.js';
+import '../imports/api/files.js';
