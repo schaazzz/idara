fec87881e33b1e327998e2d6d5b5f1a75d28c824
Shahzeb Ihsan <shahzeb@gmail.com>
2016-08-01 16:53:49 +0800

    Working on the taskboard

--- imports/ui/taskboard.js
+++ imports/ui/taskboard.js
@@ -5,6 +5,10 @@ import { Epics } from '../api/epics';
 import { Issues } from '../api/issues';
 import './taskboard.html';
 
+let dragstarted = false;
+let dragstopped = false;
+let changed = false;
+
 Template.taskboard.onRendered(function onRendered() {
     var options = {
         width: 4,
@@ -25,20 +29,11 @@ Template.taskboard.onRendered(function onRendered() {
     $('#grid-testing').gridstack(options);
     $('#grid-closed').gridstack(options);
 
-    // var items = [
-    //     {x: 0, y: 0, width: 4, height: 2},
-    //     {x: 0, y: 1, width: 4, height: 2},
-    //     {x: 0, y: 2, width: 4, height: 2},
-    //     {x: 0, y: 3, width: 4, height: 2},
-    //     {x: 0, y: 4, width: 4, height: 2}
-    // ];
-
     $('.grid-stack').each(function () {
         var grid = $(this).data('gridstack');
         var items;
         var x = 0, y = 0, w = 4, h = 2;
 
-        console.log(this.id);
         if (this.id == 'grid-new-epics') {
             items = Epics.find({'state': 'New'}).fetch();
         } else if (this.id == 'grid-icebox') {
@@ -71,10 +66,24 @@ Template.taskboard.onRendered(function onRendered() {
             var widgetContent =
                 '<strong>Title: </strong>' + node.title +
                 '<br><strong>Priority: </strong>' + node.priority +
-                '<br><strong>Assignee: </strong>' + node.responsible;
+                '<br><strong>Assignee: </strong>' + node.responsible +
+                '<br><strong>Progress: </strong>' + node.progress + '%' +
+                '<div class="row" style="padding-bottom: 0px; padding-top: 1px;">' +
+                    '<div class="col-xs-12">' +
+                        '<a href="#nolink" id="a-edit-epic" class="pull-left" style="font-size: 17px; margin-left: 0px;">' +
+                        '<span class="pull-left glyphicon glyphicon-edit" style="margin-top: 10px; color: black"></span>' +
+                        '</a>' +
+                        '<a href="#nolink" id="a-view-epic" class="pull-left" style="font-size: 17px; margin-left: 5px;">' +
+                        '<span class="pull-left glyphicon glyphicon-eye-open" style="margin-top: 10px; color: black"></span>' +
+                        '</a>' +
+                        '<a href="#nolink" id="a-delete-epic" class="pull-left" style="font-size: 17px; margin-left: 5px;">' +
+                            '<span class="pull-left glyphicon glyphicon-trash" style="margin-top: 10px; color: black"></span>' +
+                        '</a>' +
+                    '</div>' +
+                '</div>';
 
             grid.addWidget(
-                $('<div class="grid-stack-item"><div class="grid-stack-item-content small ' + itemClass + '">' + widgetContent + '</div></div>'),
+                $('<div class="grid-stack-item"' + ' id="' + node.project + ':' + node.number + '""><div class="grid-stack-item-content small ' + itemClass  + '">' + widgetContent +  '</div></div>'),
                 x, y, w, h);
                 y += 1;
         }, this);
@@ -96,6 +105,7 @@ Template.taskboard.onRendered(function onRendered() {
     });
 
     $('.grid-stack').on('change', function(event, items) {
+        console.log('change::: ', this.id);
         for (var i = 0; i < items.length; i++) {
 
         }
@@ -115,12 +125,14 @@ Template.taskboard.onRendered(function onRendered() {
     $('.grid-stack').on('dragstart', function(event, ui) {
         var grid = this;
         var element = event.target;
+        console.log('dragstart::: ', this.id, ',', element.id);
 
     });
 
     $('.grid-stack').on('dragstop', function(event, ui) {
         var grid = this;
         var element = event.target;
+        console.log('dragstop::: ', this.id, ',', element.id);
     });
     $('.grid-stack-item').draggable({
         revert: false,
